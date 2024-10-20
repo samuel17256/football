ab31b2aff3430b7a3fa5a94162c74c42412b1646
Samuel Nanbam Luka <samuelnanbam047@gmail.com>
2024-10-20 21:03:41 +0000

    initial commit

---
+++ sandbox.js
@@ -0,0 +1,11 @@
+
+function registerButton() {prompt("enter your name")
+prompt("enter your phone number")
+prompt("enter your email")
+let age = prompt("enter your age")
+let club = prompt("enter your clubname")
+prompt("enter your nationality")
+
+    age < 18 ? alert(`wait till you're 18`) : alert(`goodluck to ${club}`)
+
+}
